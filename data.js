38c7d90cae798b587e5f8e2d96022358ca5e6640
oniononionzizi-byte <oniononionzizi@gmail.com>
2026-07-01 14:47:02 +0800

    Update data.js

--- data.js
+++ data.js
@@ -11,8 +11,8 @@ const songs = [
     { id: 10, text: '主是道路真理生命 祂為我背十架' },
     { id: 14, text: '「醫治我們的神 慈愛天父 全能上帝 感謝奇妙恩典」' },
     { id: 19, text: '「祂名為耶和華創造主天父生命的源頭」' },
-    { id: 71, text: '在主前傾心敬拜祂的時候 我將一切所有完全都獻上' },
-    { id: 83, text: '讓我坦然無懼來到施恩座前 用心靈誠實尋求你' },
+    { id: 71, text: '在主前傾心敬拜「寶座」前的祂 當我將一切所有完全獻上' },
+    { id: 83, text: '讓我坦然無懼來到主「寶座」的施恩座前 用心靈誠實尋求你' },
     { id: 162, text: '『信實全能的真神公義聖潔之主祂是正直良善』' },
     { id: 8007, text: 'O Holy night The stars are brightly shining It is the night of our dear Saviour’s birth' },
     { id: 8016, text: 'Truly He taught us Love for one another His law is love and His gospel is peace' }
