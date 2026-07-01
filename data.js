e86fcc88fd44ed019a4230f54ea9d10540c109ac
oniononionzizi-byte <oniononionzizi@gmail.com>
2026-07-01 15:45:19 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const songs = [
+    { id: 1, text: '萬國都要來讚美主萬民都要來讚美主' },
+    { id: 2, text: '敬拜耶和華 唱詩來向祂歡呼讚美耶和華 萬民都快樂歡呼' },
+    { id: 3, text: '敬拜讚美耶和華 凡有氣息都要讚美祂' },
+    { id: 4, text: '敬拜耶和華 讚美耶和華' },
+    { id: 5, text: '「我們活著是為要 敬拜讚美祂」' },
+    { id: 6, text: '來向主歌唱 向主歡呼感謝讚美主耶穌' },
+    { id: 7, text: '因祂本為大 聖潔尊貴 在萬神之上' },
+    { id: 8, text: '要頌讚主聖名 惟有主配得讚美' },
+    { id: 9, text: '「從永遠到永遠我們尊崇主聖名」' },
+    { id: 71, text: '在主前傾心敬拜「寶座」前的祂 當我將一切所有完全獻上' },
+    { id: 83, text: '讓我坦然無懼來到主「寶座」的施恩座前 用心靈誠實尋求你' },
+    { id: 162, text: '『信實全能的真神公義聖潔之主祂是正直良善』' },
+    { id: 204, text: '向萬王之王抬起頭仰望虔誠敬拜歌唱讚美祂' },
+    { id: 8007, text: 'O Holy night The stars are brightly shining It is the night of our dear Saviour’s birth' },
+    { id: 8016, text: 'Truly He taught us Love for one another His law is love and His gospel is peace' }
+];
